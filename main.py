bdfab64fcf8cc0e3c3b28f80e13ec95da4a150c4
LiamTheMagician <liamosullivan.pro@protonmail.com>
2024-12-07 10:21:45 +0100

    Test 2

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-#comment for me github fans out there i recognize you
-import sys
-import pygame as pg
+import sys, pygame as pg
 from pygame.locals import *
-from src.header import *
 
+SCREEN_SIZE = (1280, 720)
+
+class Game:
+    def __init__(self):
+        self.window = pg.display.set_mode(SCREEN_SIZE)
+        self.running = True
+
+    def handleEvents(self):
+        for event in pg.event.get():
+            if event.type == QUIT:
+                pg.quit()
+                sys.exit()
+                running = False
+
+    def run(self):
+        running = True
+        while self.running:
+            self.handleEvents()
+            
 game = Game()
 game.run()
