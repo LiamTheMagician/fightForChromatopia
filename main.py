cc00b9451d874a38d9f9151dfa34dcc56d07b318
LiamTheMagician <liamosullivan.pro@protonmail.com>
2025-01-29 19:51:32 +0100

    first collisions tests

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
+screen = pygame.display.set_mode((720,480))
 
 player_surf = pygame.image.load('art/player.png').convert_alpha()
 player_rect = player_surf.get_rect(topleft = (200, 200))
@@ -11,14 +11,23 @@ block_surf = pygame.Surface((50,50))
 clock = pygame.time.Clock()
 
 def AABB(a, b):
-    if ((b.right > a.right > b.left) and (b.left < a.left < b.right)) or ((b.bottom > a.bottom > b.top) and (b.top < a.top < b.bottom)):
-        return True
+    if a.left > b.right:
+        return False
+    if a.top < b.bottom:
+        return False
+    if a.right < b.left:
+        return False
+    if a.bottom < b.top:
+        return False
+    
+        
 while True:
     screen.fill((50,50,50))
     block_rect = pygame.draw.rect(screen, (255, 255, 255), pygame.Rect((1280-100)/2, (720-100)/2, 100, 100))
     screen.blit(player_surf, player_rect)
-    print(AABB(player_rect, block_rect))
     
+    print(AABB(player_rect, block_rect))
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         player_rect.y -= 4
