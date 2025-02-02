edefb701379a207bd43d7d042036902ac4f0e6ea
theo_ti_me <hamardtheotime@gmail.com>
2025-02-02 17:29:36 +0100

    menu nulle

--- teste_menu.py
+++ teste_menu.py
@@ -7,8 +7,6 @@ fenetre = pygame.display.set_mode((640,480))
 
 inventaire = pygame.Rect(200,120,50,50)
 pygame.draw.rect(fenetre,(50,50,50),inventaire)
-position_inventaire = (200,120)
-fenetre.blit(inventaire,position_inventaire)
 
 
 continuer = True
@@ -19,9 +17,8 @@ while continuer:
         
         if event.type == KEYDOWN:
             if event.key == K_m:
-                position_inventaire = position_inventaire(300,220)
+                inventaire = pygame.Rect(300,220,50,50)
 
-    fenetre.blit(inventaire,position_inventaire)
     pygame.draw.rect(fenetre,(50,50,50),inventaire)
     pygame.display.flip()
 
