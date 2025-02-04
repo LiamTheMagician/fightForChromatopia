5bce04880bdd00157e3a9f876edc42dd71a13dd5
LiamTheMagician <liamosullivan.pro@protonmail.com>
2025-02-04 15:05:07 +0100

    collisions + others

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
 import pygame
 from player import *
-from map import *
+from map    import *
 from object import *
 
 pygame.init()
 screen = pygame.display.set_mode((720,480))
 clock = pygame.time.Clock()
 
-M =[['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
-    ['X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
-    ['X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
-    ['X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
-    ['X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
-    ['X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
-    ['X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
-    ['X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']]
-
-
-o_group = pygame.sprite.Group()
-object_mapping(M, o_group)
-
-p1 = Player(3, o_group, screen)
+p1 = Player(3, [], screen)
 p_group = pygame.sprite.Group(p1)
 
 while True:
@@ -34,9 +19,6 @@ while True:
     screen.fill((50,50,50))
     
     #Draw
-    o_group.draw(screen)
-    o_group.update()
-
     p_group.draw(screen)
     p_group.update()
 
--- map.py
+++ map.py
@@ -1,13 +1,16 @@
 import pygame
 from object import *
+from player import *
 
 GRID_SIZE = 50
 #'O' = ouvert
 #'X' = fermé
 
-def object_mapping(mat,group):
+def object_mapping(mat,collision_group, player_group):
     for i in range(len(mat)):
         for j in range(len(mat[i])):
             if mat[i][j] == 'X':
-               o = Object((j*GRID_SIZE, i*GRID_SIZE))
-               group.add(o)
+                o = Object((j*GRID_SIZE, i*GRID_SIZE))
+                group.add(o)
+            if mat[i][j] == 'P':
+                p = Player(5, collision_group)
